9a6f641cf10324928faee1cd0359285aef753bf4
rukmanary <rukmanary@gmail.com>
2026-06-24 11:12:53 +0700

    first init

---
+++ app/src/main/java/com/rkmn/introtocompose/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.rkmn.introtocompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.rkmn.introtocompose.ui.theme.IntroToComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            IntroToComposeTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(name = "Android", modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(text = "Hello $name!", modifier = modifier)
+}
+
+@Preview
+@Composable
+fun ShowAge(age: Int = 12) {
+    Text(text = age.toString())
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    IntroToComposeTheme {
+        Column() {
+            Greeting("Android")
+            ShowAge(24)
+        }
+    }
+}
